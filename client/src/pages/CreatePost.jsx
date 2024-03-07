42de8618c9894c18ab74db02b12f94beebe05bd4
Felicia <feliciamihaela2@gmail.com>
2024-03-08 00:35:28 +0200

    CreatePost validations

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Formik, Form, Field, ErrorMessage} from 'formik'
+import * as Yup from 'yup'
 
 function CreatePost() {
 
@@ -9,26 +10,35 @@ function CreatePost() {
         username: "",
     };
 
+    const validationSchema=Yup.object().shape({
+        title: Yup.string().required(),
+        text: Yup.string().required(),
+        username: Yup.string().min(3).max(20).required()
+    })
+
     const onSubmit=(data)=>{
         console.log(data);
     }
   return (
     <div className='createPostContainer'> 
-        <Formik initialValues={initialValues} onSubmit={onSubmit}>
+        <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
         <Form>
             <label>Title: </label>
+            <ErrorMessage name="title" component ="span"/>
             <Field  
             id="inputCreatePost" 
             name="title" 
             placeholder="Write a title"
             />
             <label>Post: </label>
+            <ErrorMessage name="text" component ="span"/>
             <Field  
             id="inputCreatePost" 
             name="text" 
             placeholder="Write a post"
             />
             <label>Username: </label>
+            <ErrorMessage name="username" component ="span"/>
             <Field  
             id="inputCreatePost" 
             name="username" 
