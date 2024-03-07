d01ca2bb8a9199583aa43fd428f45d691b29d9b7
Felicia <feliciamihaela2@gmail.com>
2024-03-08 00:46:26 +0200

    Submitiong posts to the posts page is working

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Formik, Form, Field, ErrorMessage} from 'formik'
 import * as Yup from 'yup'
+import axios from 'axios'
 
 function CreatePost() {
 
@@ -17,7 +18,10 @@ function CreatePost() {
     })
 
     const onSubmit=(data)=>{
-        console.log(data);
+        axios.post("http://localhost:3001/posts", data).then((response)=>{
+           
+        });
+       
     }
   return (
     <div className='createPostContainer'> 
