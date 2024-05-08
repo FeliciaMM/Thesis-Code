6d6ea5fed8deeed18034c1f92d6c65a5063c9447
Felicia <feliciamihaela2@gmail.com>
2024-05-08 22:04:50 +0300

    profile page frontend (link to acces profile needed)

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,16 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import{useParams} from 'react-router-dom';
+import{useParams, useNavigate} from 'react-router-dom';
 import axios from 'axios';
 
 export default function Profile() {
   let{id} = useParams();
+  let navigate = useNavigate();
   const [username, setUsername] = useState("");
+  const [listOfPosts, setlistOfPosts] = useState([]);
 
     useEffect(()=>{
       axios.get(`http://localhost:3001/authentification/basicinfo/${id}`).then((response) => {
         setUsername(response.data.username);
       });
 
+      axios.get(`http://localhost:3001/posts/byUserId/${id}`).then((response)=>{
+        setlistOfPosts(response.data);
+      })
     },[id]);
 
   return (
@@ -18,7 +23,19 @@ export default function Profile() {
       <div className='basicInfo'>
         <h1>Username: {username}</h1>
       </div>
-      <div className='listOfPosts'></div>
+      <div className='listOfPosts'>
+      <div>
+      <ul>
+        {listOfPosts.map((post, index) => (
+          <div className='post' key={index} onClick={() => { navigate(`/post/${post.id}`) }}>
+            <div className='title'>{post.title}</div>
+            <div className='body'>{post.text}</div>
+            <div className='username'>{post.username}</div>
+          </div>
+        ))}
+      </ul>
+    </div>
+      </div>
     </div>
   )
 }
