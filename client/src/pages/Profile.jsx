61e4b84c9fb12ee2d412286f42aa5df1258d19c7
Felicia <feliciamihaela2@gmail.com>
2024-05-13 16:59:19 +0300

    small changes

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,27 +1,40 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import{useParams, useNavigate} from 'react-router-dom';
 import axios from 'axios';
+import { AuthContext } from '../helpers/AuthContext';
 
 export default function Profile() {
   let{id} = useParams();
   let navigate = useNavigate();
   const [username, setUsername] = useState("");
   const [listOfPosts, setlistOfPosts] = useState([]);
+  const {authState} = useContext(AuthContext);
+  const [profilePicture, setProfilePicture] = useState(null);
 
     useEffect(()=>{
       axios.get(`http://localhost:3001/authentification/basicinfo/${id}`).then((response) => {
         setUsername(response.data.username);
+        setProfilePicture(response.data.profile_picture);
       });
 
       axios.get(`http://localhost:3001/posts/byUserId/${id}`).then((response)=>{
         setlistOfPosts(response.data);
       })
     },[id]);
+    
 
   return (
     <div className='profilePageContainer'>
       <div className='basicInfo'>
         <h1>Username: {username}</h1>
+        <div className='profilePictureChange'> {authState.username === username && (
+              <button>
+                {" "}
+                Change Picture
+                
+              </button>
+            )}
+            </div>
       </div>
       <div className='listOfPosts'>
       <div>
